bdc829a920eb7e42d7fe058140ec4e9185f68dce
Kadiss Ibrahim <ibrahimaitkadiss@gmail.com>
2026-07-15 14:49:02 +0100

    Auto-Redirection to login of not logged in, and guest conversely

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './shared/components/layout/layout.component';
 import { ManufacturerListComponent } from './features/manufacturers/manufacturer-list/manufacturer-list.component';
 import { LoginComponent } from './features/auth/login/login.component';
+import { authGuard } from './core/guards/auth.guard';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent , canActivate: [guestGuard]  },
   {
     path: '',
     component: LayoutComponent,
+    canActivate: [authGuard],
     children: [
       { path: '', redirectTo: 'devices', pathMatch: 'full' },
       { path: 'manufacturers', component: ManufacturerListComponent },
